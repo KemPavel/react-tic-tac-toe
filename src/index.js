f66f9897ef4e770d940f032244dec08b7a010adb
Pavel_Kazakov <Pavel_Kazakov@epam.com>
2018-08-10 18:12:07 +0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TicTacToeGame from './containers/TicTacToeGame';
+import './style.css';
+
+
+ReactDOM.render(
+  <TicTacToeGame />,
+  document.getElementById('root')
+);
